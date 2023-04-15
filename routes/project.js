2d0ee9cce2b8fab6d3c8301b4a6e4c177e5f9a4a
mdZaid165201b <107930247+mdZaid165201b@users.noreply.github.com>
2023-04-15 09:23:41 +0500

    routes cleanup

--- routes/project.js
+++ routes/project.js
@@ -4,29 +4,6 @@ const makeUploadDir = require("../middleware/makeDirectory");
 const {createProject, updateProject, deleteProject, findProject, findProjects} = require("../controller/project");
 const multerMiddleware = require("../middleware/multerMiddleware");
 
-let storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "./tmp");
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + file.originalname);
-    },
-});
-const filefilter = (req, file, cb) => {
-    if (
-        file.mimetype == "image/jpg" ||
-        file.mimetype == "image/jpeg" ||
-        file.mimetype == "image/png"
-    ) {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
-};
-let upload = multer({
-    storage: storage,
-    fileFilter: filefilter,
-});
 
 router.post("/create-project", makeUploadDir, multerMiddleware.array("image", 10), createProject);
 
--- routes/user.js
+++ routes/user.js
@@ -3,29 +3,7 @@ const { register, login, updateUser, findUser } = require("../controller/user");
 const multer = require("multer");
 const makeUploadDir = require("../middleware/makeDirectory");
 const multerMiddleware = require("../middleware/multerMiddleware");
-let storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "./tmp");
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + file.originalname);
-    },
-});
-const filefilter = (req, file, cb) => {
-    if (
-        file.mimetype == "image/jpg" ||
-        file.mimetype == "image/jpeg" ||
-        file.mimetype == "image/png"
-    ) {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
-};
-let upload = multer({
-    storage: storage,
-    fileFilter: filefilter,
-});
+
 
 
 router.post("/register", register);
