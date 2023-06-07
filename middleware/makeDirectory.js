bac0b1a434cc10f5d50348142446d4f6e113b59c
mdZaid165201b <107930247+mdZaid165201b@users.noreply.github.com>
2023-06-07 13:01:47 +0500

    makeDirectory change

--- middleware/makeDirectory.js
+++ middleware/makeDirectory.js
@@ -2,7 +2,9 @@ const fs = require("fs");
 
 const makeDirectory = (req, res, next) => {
     if(!fs.existsSync("./tmp")){
-        fs.mkdirSync("./tmp", {recursive: true})
+        fs.mkdirSync("./tmp", {recursive: true}, (err) => {
+            console.log(err)
+        })
     }
     next();
 }
