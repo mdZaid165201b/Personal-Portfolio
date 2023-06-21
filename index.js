5975cc6f2fb213dbec36cec1769294990ecb421f
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2023-06-21 09:57:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cors());
 app.use(fileupload({
   useTempFiles: true,
-  tempFileDir: "/tmp",
+  tempFileDir: "./tmp",
 }))
 
 // ------------------------------------------routes registration-----------------------------------
